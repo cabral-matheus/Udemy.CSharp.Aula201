9927867aa5ec2e8686baaf729aff07d62d42e001
Matheus Cabral <matheuscabraldesousa@gmail.com>
2023-06-22 16:18:02 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Code for an activity proposed by the C# course on the udemy.com website.
+
+Make a program to read the path of a .csv file containing the data of items sold. Each item has a name, unit price and quantity, separated by a comma. You must generate a new file called "summary.csv", located in a subfolder called "out" from the original folder of the source file, containing only the name and total value for that item (unit price multiplied by quantity), as per example:
+
+Source file:
+TV LED,1290.99,1
+Video Game Chair,350.50,3
+Iphone X,900.00,2
+Samsung Galaxy 9,850.00,2
+
+Output file(\out\summary.csv):
+TV LED,1290.99
+Video Game Chair,1051.50
+Iphone X,1800.00
+Samsung Galaxy 9,1700.00
---
+++ Udemy.CSharp.Aula201/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using Udemy.CSharp.Aula201.Entities;
+
+namespace Udemy.CSharp.Aula201
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file full path");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string sourceDirectoryPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceDirectoryPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                string[] sourceFileItens = File.ReadAllLines(sourceFilePath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    ;
+                    foreach (string line in sourceFileItens)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product product = new Product(name, price, quantity);
+
+                        sw.WriteLine(product.Name + "," + product.TotalValue().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            } catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
